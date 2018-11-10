483cd7719aa3eaff9452ffe86ca18b126462ed52
George Ionov <newinventor20@gmail.com>
2018-11-10 19:27:52 +0400

    get hash

--- src/Controller/WebApiController.php
+++ src/Controller/WebApiController.php
@@ -11,6 +11,7 @@ namespace App\Controller;
 
 use App\Entity\ObjectParameter;
 use Doctrine\ORM\EntityManagerInterface;
+use Doctrine\ORM\Query\ResultSetMapping;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 
@@ -28,7 +29,7 @@ class WebApiController
 
     public function getUpdates(Request $request)
     {
-        var_dump($request->query);
+        var_dump($request->getContent());
         /** @var ObjectParameter[] $parameter */
         $parameter = $this->em->createQueryBuilder()
             ->select('op')
@@ -39,18 +40,16 @@ class WebApiController
             ->getResult();
         if (\count($parameter) > 0) {
             $var = '12123123123';
-            $storedProcedureSQL = "SELECT match_object_hash(:hash) as result;";
-            $conn = $this->em->getConnection();
-            $stmt = $conn->prepare($storedProcedureSQL);
-            $stmt->bindParam(':hash', $var);
-            $res = $stmt->fetchAll();
+            $rsm = new ResultSetMapping();
+            $rsm->addScalarResult('result', 'result');
+            $query = $this->em->createNativeQuery('SELECT match_object_hash(?) as result;', $rsm);
+            $query->setParameter(1, $var);
+            $res = $query->getResult()[0]['result'];
             var_dump($res);
-            if($res['item'] === 0){
+            if($res === 0){
                 return new JsonResponse(['json' => ['updates' => 'has updates', 'object_id' => $parameter[0]->getValue()]], 200);
             }
             return new JsonResponse(['json' => ['updates' => 'no updates', 'object_id' => $parameter[0]->getValue()]], 200);
-//            return new JsonResponse(['json' => ['updates' => 'no updates', 'object_id' => $parameter[0]->getValue()]],
-//                200);
         }
         return new JsonResponse(['result' => 'error', 'message' => 'Object did not initialised.'], 200);
     }
